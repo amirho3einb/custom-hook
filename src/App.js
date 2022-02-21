02fa3f2efa1b7c74b3a0c366e809a317d1a0acb1
Amirhossein <amirhossinbalagar@gmail.com>
2022-02-21 18:59:12 +0330

    fetchData custom hook

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import useFetch from "./hook/useFetch";
+
+function App() {
+  const { error, loading, data } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  return (
+    <div className="App">
+      {loading && <p>loadig...</p>}
+      {error && <p>{error}</p>}
+      {data && (
+        <div>
+          {data.map((user) => (
+            <li>{user.name}</li>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
